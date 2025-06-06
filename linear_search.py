d924cca1cf015621b86bdfa1d960fd08b610ca40
czarnick89 <nso83@hotmail.com>
2025-06-05 19:02:46 -0500

    working searches and additional tests

--- linear_search.py
+++ linear_search.py
@@ -1,7 +1,23 @@
 def linear_search(value_to_find, array_to_search_through):
-    # your code here
-    pass
+
+    idx = 0
+    for item in array_to_search_through:
+        if item == value_to_find:
+            return idx
+        idx+=1
+    
+    return None
 
 def linear_search_global(value_to_find, array_to_search_through):
-    # your code here
-    pass
+    
+    idx = 0
+    returnArr = []
+    for item in array_to_search_through:
+        if item == value_to_find:
+            returnArr.append(idx)
+        idx+=1
+
+    if len(returnArr)>0:
+        return returnArr
+    
+    return None
